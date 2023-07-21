9d9cb66442185d9aae1f93d6ee5ecac41a33ed7a
Cholpon Zhanybek uulu <armiiasova.pl@gmail.com>
2023-07-21 18:51:01 +0200

    changes added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const currencyEl_one = document.getElementById('currency-one');
-
 const amountEl_one = document.getElementById('amount-one');
-
 const currencyEl_two = document.getElementById('currency-two');
-
-
 const amountEl_two = document.getElementById('amount-two');
 
-function calculate() {}
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//Fetch exchange rates and update the DOM
+function calculate() {
+  console.log('RAN');
+}
+
+//Event listeners
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate)
 
 calculate();
